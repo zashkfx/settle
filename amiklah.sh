d690e9baf8e106e66af7b52f66fc578f34cfbbe7
zashkfx <78897006+zashkfx@users.noreply.github.com>
2021-07-27 19:18:34 +0800

    Update amiklah.sh

--- amiklah.sh
+++ amiklah.sh
@@ -3,10 +3,6 @@ if [ "${EUID}" -ne 0 ]; then
 		echo "You need to run this script as root"
 		exit 1
 fi
-if [ "$(systemd-detect-virt)" == "openvz" ]; then
-		echo "OpenVZ is not supported"
-		exit 1
-fi
 red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
